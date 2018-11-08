812c7ed0b2b963e4b94ff1b2d6bac164d6a42eb7
Max Rosen <maxcrosen@gmail.com>
2018-11-08 17:11:20 -0500

    this is the first true commitment

---
+++ index.js
@@ -0,0 +1,6 @@
+// require these files links the files in the programs
+var Letters = require("./Letters");
+var Words = require("./words.js");
+
+//Activates the Letters constructor
+var StartGame = new Letters();
